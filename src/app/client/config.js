23c0208908c073251e70b2b3c57e206a20018e07
Divjot Singh <bogas04@gmail.com>
2016-04-11 01:44:16 +0530

    add search options to config

--- src/app/client/config.js
+++ src/app/client/config.js
@@ -6,4 +6,15 @@ export default {
     { title: 'Contact', url: '/contact' },
   ],
   FAVICON: 'http://dogeminer.se/favicon.ico', 
+  search: [
+    { by: 'rating', type: 'linear', }, // overall rating
+    { by: 'title', type: 'string', }, // title like
+    { by: 'description', type: 'string', }, // description like
+    { by: 'location', type: 'geo', }, // nearness to location
+    { by: 'reviews', type: 'count', }, // count of reviews
+    { by: 'bookmarks', type: 'count', }, // count of bookmarks
+    { by: 'sentiment', type: 'linear', }, // overall sentiment of reviews
+    { by: 'activity', type: 'recency', }, // order by updatedAt, createdAt of reviews, bookmarks and ratings
+    { by: 'new', type: 'recency', }, // order by createdAt of entities
+  ],
 };
--- src/app/server/config.js
+++ src/app/server/config.js
@@ -14,4 +14,15 @@ export default {
   JWT_SECRET: 'QWERTYUIOP~!@#$%^&*()_+ASDFGHJKL:"~!@#$%^&*()_+ZXCVBNM<>?~!@#$%^&*()_+"',
   JWT_EXPIRES_IN: '30 days',
   FAVICON: 'http://dogeminer.se/favicon.ico', 
+  search: [
+    { by: 'rating', type: 'linear', }, // overall rating
+    { by: 'title', type: 'string', }, // title like
+    { by: 'description', type: 'string', }, // description like
+    { by: 'location', type: 'geo', }, // nearness to location
+    { by: 'reviews', type: 'count', }, // count of reviews
+    { by: 'bookmarks', type: 'count', }, // count of bookmarks
+    { by: 'sentiment', type: 'linear', }, // overall sentiment of reviews
+    { by: 'activity', type: 'recency', }, // order by updatedAt, createdAt of reviews, bookmarks and ratings
+    { by: 'new', type: 'recency', }, // order by createdAt of entities
+  ],
 };
