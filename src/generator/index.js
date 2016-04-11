49206cb1b7fb2d7db10910c65566b2a3fdc5980f
akanshi <akanshi.g.2404@gmail.com>
2016-04-12 00:50:50 +0530

    generator UI inquirer

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,46 +1,82 @@
-const fs = require('fs-extra-promise');
-const dir2json = require('jsondir').dir2json;
+'use strict';
+var inquirer = require('inquirer');
 
-try {
-  const options = require('./options.json');
-} catch (e) {
-  throw new Error("options.json file not found");
-}
-dir2json('../app', (err, results) => {
-  if (err) throw err;
-  delete results.node_modules;
-  delete results.web.node_modules;
-  console.log(results);
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'app_name',
+    message: 'Enter the name of your app'
+  },
+  {
+    type: 'input',
+    name: 'entity_name',
+    message: 'Enter the entity name'
+  },
+  {
+    type: 'checkbox',
+    message: 'Select the theme for the app',
+    name: 'theme',
+    choices: [
+      {
+        name: 'Flatly'
+        //have to see how to set any theme to default
+      },
+      {
+        name: 'Darkly'
+      },
+      {
+        name: 'Theme0'
+      },
+      {
+        name: 'Theme1'
+      }
+    ],
+    validate: function (answer) {
+      if (answer.length === 1) {
+        return true;
+      }
+      return 'Choose atmost one theme.';
+    }
+  },
+  {
+    type: 'checkbox',
+    message: 'Select the database you want to use',
+    name: 'db',
+    choices: [
+      {
+        name: 'mysql'
+      },
+      {
+        name: 'postgresql'
+      },
+      {
+        name: 'mongodb'
+      },
+      {
+        name: 'db0'
+      }
+      //have to add option for others(not available in options)
+    ],
+    validate: function (answer) {
+      if (answer.length === 1) {
+        return true;
+        //have to see if no option is selected
+      }
+      return 'Choose atmost one database option';
+    }
+  },
+  {
+    type: 'input',
+    name: 'logoPath',
+    message: 'Enter the path of your logo (__(default./logo.png)_____)'
+  },
+  {
+    type: 'input',
+    name: 'entitySchemaPath',
+    message: 'Enter the path of schema.js for entity (_(default ./schema.js)_______)',
+    }
+]).then(function (answers) {
+  console.log(JSON.stringify(answers, null, '  '));
 });
 
-//fs.copyAsync('../app', __dirname)
-//.then(status => console.log(status))
-//.catch(e => console.log(e));
 
-// DONE fs.copy('../app', __dirname)                             // copy skeleton
-//.then(() => require('./modifyDatabase')(options.schema)) // generates migration files
-//.then(require('./updateServer'))                         // update server APIs
-//.then(require('./injectComponents'))                     // replace default components with user defined components
-//.then(require('./updateComponents'))                     // update components to consume new API
-//.then(() => require('./injectStyle')(options.style))     // add style options chosen by user
-//.catch(reportError);
-
-
-/*
-   $ npm i mvpgen -g
-   $ mkdir ProjectFolder
-   $ cd ProjectFolder
-   $ touch options.js
-   $ echo "asdasd" >> options.js
-   $ mvpgen
-   >> json Options file 
-   >> [*] copy entire project folder
-   >> >> Modify schema (db.js) (INNOVATION)
-   >> >> app/server/db/ <- variety/db-mongo
-   >> >> app/components/Rating <- {options.components.rating.location}
-   ...
-   >> final optimizations
-   >> "Project ready, do npm start to run server and visit http://localhost:1337"
-   >> "Mobile apps are found in apps/iOS and app/Android"
-
-*/
